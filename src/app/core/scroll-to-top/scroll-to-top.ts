b8f1c1418ae1eff4acc6f09e975d74251cee2839
Melodious-nub <shawon.talukder1@gmail.com>
2026-01-20 10:35:35 +0600

    updated with perfectly

--- src/app/core/scroll-to-top/scroll-to-top.ts
+++ src/app/core/scroll-to-top/scroll-to-top.ts
@@ -8,7 +8,7 @@ import { Component, HostListener } from '@angular/core';
       [class.show]="showButton"
       (click)="scrollToTop()"
       aria-label="Scroll to top">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor">
+      <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
       </svg>
     </button>
