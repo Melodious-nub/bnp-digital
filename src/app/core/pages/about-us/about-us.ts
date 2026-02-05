192cd3ca09098b4ec230362f0eadd29eeb977581
Melodious-nub <shawon.talukder1@gmail.com>
2026-02-05 23:42:51 +0600

    updated with footer and new content and photo frame

--- src/app/core/pages/about-us/about-us.ts
+++ src/app/core/pages/about-us/about-us.ts
@@ -7,7 +7,7 @@ interface TeamMember {
     name: string;
     role: string;
     photoUrl: string;
-    facebookLink: string;
+    facebookLink?: string;
     linkedinLink?: string;
 }
 
@@ -27,56 +27,91 @@ export class AboutUsComponent implements OnInit {
 
     teams: TeamCategory[] = [
         {
-            title: 'ওয়েবসাইট টিম',
+            title: 'উপদেষ্টা',
             members: [
                 {
-                    name: 'এস. এম. মাহমুদুল হায়দার (মীম)',
-                    role: 'ওয়েবসাইট টিম',
+                    name: 'ব্যারিস্টার এম. এ. সালাম',
+                    role: 'উপদেষ্টা',
+                    photoUrl: '/teams/Barrister_MA_Salam.png',
+                    facebookLink: 'https://www.facebook.com/barristermasalam'
+                },
+                {
+                    name: 'ড. মাহদী আমিন',
+                    role: 'উপদেষ্টা',
+                    photoUrl: '/teams/mahdi_amin.png',
+                    facebookLink: 'https://www.facebook.com/share/186irNseG3/?mibextid=wwXIfr'
+                },
+                {
+                    name: 'ড. সালেহ শিবলী',
+                    role: 'উপদেষ্টা',
+                    photoUrl: '/teams/Dr_Saleh_Shibly.PNG',
+                    facebookLink: 'https://www.facebook.com/share/16aEYhqokY/?mibextid=wwXIfr'
+                },
+                {
+                    name: 'রেহান এ আসাদ',
+                    role: 'উপদেষ্টা',
+                    photoUrl: '/teams/Rehan_Asad.PNG',
+                    linkedinLink: 'https://www.linkedin.com/in/rasad'
+                },
+                {
+                    name: 'এইচ এম সাইফ আলী খান',
+                    role: 'উপদেষ্টা',
+                    photoUrl: '/teams/HM_Saif_Ali_Khan.PNG',
+                    facebookLink: 'https://www.facebook.com/share/1JJQdeSxda/?mibextid=wwXIfr'
+                },
+                {
+                    name: 'ব্যারিস্টার শরীফ এম. এস. হায়দার',
+                    role: 'উপদেষ্টা',
+                    photoUrl: '/teams/Barrister_Sharif_Hyder.png',
+                    facebookLink: 'https://www.facebook.com/smshyder'
+                }
+            ]
+        },
+        {
+            title: 'রাজনৈতিক সমন্বয় ও যোগাযোগ',
+            members: [
+                {
+                    name: 'শফিকুল ইসলাম রিবলু',
+                    role: 'প্রধান সমন্বয়ক',
+                    photoUrl: '/teams/MdShafikul_Islam_Riblu.png',
+                    facebookLink: 'https://www.facebook.com/mdshafikul.islam.73'
+                },
+                {
+                    name: 'আসিফ উল ইসলাম',
+                    role: 'সহকারী সমন্বয়ক',
+                    photoUrl: '/teams/Aasif_Ul_Islam.PNG',
+                    facebookLink: 'https://www.facebook.com/share/1SHTvM5Edr/?mibextid=wwXIfr'
+                }
+            ]
+        },
+        {
+            title: 'কারিগরি বাস্তবায়ন',
+            members: [
+                {
+                    name: 'এস. এম. মাহমুদুল হায়দার (মীম)',
+                    role: 'কারিগরি বাস্তবায়ন',
                     photoUrl: '/teams/SM_Mahmudul_Hyder.png',
                     facebookLink: 'https://www.facebook.com/smmhyder/',
                     linkedinLink: 'https://www.linkedin.com/in/smmhyder/'
                 },
                 {
                     name: 'তানজিল চৌধুরী',
-                    role: 'ওয়েবসাইট টিম',
+                    role: 'কারিগরি বাস্তবায়ন',
                     photoUrl: '/teams/Chowdhury_Tanjil.png',
                     facebookLink: 'https://www.facebook.com/tjc1971',
                     linkedinLink: 'https://www.linkedin.com/in/tanjil1971'
                 },
                 {
                     name: 'সেলিম মাহমুদ',
-                    role: 'ওয়েবসাইট টিম',
+                    role: 'কারিগরি বাস্তবায়ন',
                     photoUrl: '/teams/Salim_Mahmud.png',
                     facebookLink: 'https://www.facebook.com/salim.mahmud.7758'
                 },
                 {
                     name: 'জুল আফরোজ মজুমদার',
-                    role: 'ওয়েবসাইট টিম',
+                    role: 'কারিগরি বাস্তবায়ন',
                     photoUrl: '/teams/Zul_Afros.png',
                     facebookLink: 'https://www.facebook.com/zul.afros'
-                }
-            ]
-        },
-        {
-            title: 'উপদেষ্টা এবং পৃষ্ঠপোষক',
-            members: [
-                {
-                    name: 'ব্যারিস্টার এম এ সালাম',
-                    role: 'উপদেষ্টা এবং পৃষ্ঠপোষক',
-                    photoUrl: '/teams/Barrister_MA_Salam.png',
-                    facebookLink: 'https://www.facebook.com/barristermasalam'
-                },
-                {
-                    name: 'ব্যারিস্টার শরীফ হায়দার (মৃদুল)',
-                    role: 'উপদেষ্টা এবং পৃষ্ঠপোষক',
-                    photoUrl: '/teams/SharifHyder.png',
-                    facebookLink: 'https://www.facebook.com/smshyder'
-                },
-                {
-                    name: 'শফিকুল ইসলাম রিবলু',
-                    role: 'উপদেষ্টা এবং পৃষ্ঠপোষক',
-                    photoUrl: '/teams/MdShafikul_Islam_Riblu.png',
-                    facebookLink: 'https://www.facebook.com/mdshafikul.islam.73'
                 }
             ]
         }
